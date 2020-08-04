78da3bc7491f25591bb93fef8594407411c731fc
Skinny Coders <skinnycoders@gmail.com>
2020-08-04 13:24:10 +0700

    test

--- application/views/v_home/v_home.php
+++ application/views/v_home/v_home.php
@@ -42,7 +42,7 @@
         <header class="masthead bg-primary text-white text-center">
             <div class="container d-flex align-items-center flex-column">
                 <!-- Masthead Avatar Image--><img class="masthead-avatar mb-5" src="<?=base_url('assets/images/logo_sd.png')?>" alt="" /><!-- Masthead Heading-->
-                <h1 class="masthead-heading text-uppercase mb-0">SD KARITAS NANDAN</h1>
+                <h1 class="masthead-heading text-uppercase mb-0">SD KARITAS NANDAN 1</h1>
                 <!-- Icon Divider-->
                 <div class="divider-custom divider-light">
                     <div class="divider-custom-line"></div>
